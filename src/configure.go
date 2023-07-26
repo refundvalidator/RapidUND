585ac2cf330ab93a3fc487dc270976adadc60ff1
refundvalidator <refundvalidator@gmail.com>
2023-07-26 18:17:38 -0500

    edit

--- src/configure.go
+++ src/configure.go
@@ -10,13 +10,21 @@ type Pruning struct {
     pruning_every int
     pruning_interval int
 }
+type StateSync struct {
+    enabled bool
+    height int
+    hash string
+}
 type InstallPlan struct {
     install_preset string
     chain string
     und_url string
     cosmovisor_url string
+    und_version string
+    cosmovisor_version string
     moniker string
     Pruning
+    StateSync
 }
 // func (i InstallPlan) setPruning( p Pruning){
     // i.Pruning = p
